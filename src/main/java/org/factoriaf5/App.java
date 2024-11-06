6cd5ffaf247d013dac01059d3220fe325230a2a2
Andres Vazquez <andres.vazquez.martin@gmail.com>
2024-11-06 22:36:29 +0100

    target folder removed from repository

--- src/main/java/org/factoriaf5/App.java
+++ src/main/java/org/factoriaf5/App.java
@@ -1,13 +1,19 @@
 package org.factoriaf5;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduce un numero:");
+        int inputNumber = sc.nextInt();
+
+        // Llama al método fizzBuzz de la clase FizzBuzz
+        System.out.println(fizzBuzz.fizzBuzz(inputNumber));
+
+        sc.close();
     }
 }
--- src/test/java/org/factoriaf5/AppTest.java
+++ src/test/java/org/factoriaf5/AppTest.java
@@ -1,20 +1,41 @@
 package org.factoriaf5;
 
-import static org.junit.Assert.assertTrue;
-
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+
+    FizzBuzz fizzBuzz = new FizzBuzz();
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testFizzBuzz() {
+        assertEquals("FizzBuzz", fizzBuzz.fizzBuzz(15));
+    }
+
+    @Test
+    public void testFizz() {
+        assertEquals("Fizz", fizzBuzz.fizzBuzz(9));
+        assertEquals("Fizz", fizzBuzz.fizzBuzz(13));
+    }
+
+    @Test
+    public void testBuzz() {
+        assertEquals("Buzz", fizzBuzz.fizzBuzz(10));
+        assertEquals("Buzz", fizzBuzz.fizzBuzz(52));
+    }
+
+    @Test
+    public void testNumber() {
+        assertEquals("7", fizzBuzz.fizzBuzz(7));
+    }
+
+    @Test
+    public void testZero() {
+        assertEquals("FizzBuzz", fizzBuzz.fizzBuzz(0));
+    }
+
+    @Test
+    public void testNegativeFizz() {
+        assertEquals("Fizz", fizzBuzz.fizzBuzz(-3));
     }
 }
